06ed755328a97171db0d512f157fbaa434a33a8a
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-26 14:28:09 +0300

    Note detail view

--- SecureNoteApp/NoteDetailView.swift
+++ SecureNoteApp/NoteDetailView.swift
@@ -10,7 +10,43 @@ import SwiftUI
 struct NoteDetailView: View {
     var note: SecureNote
     
+    @State private var showUnlock = false
+    @State private var authSuccess = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if note.isSecured {
+                if !authSuccess {
+                    VStack {
+                        Text("This is a secured note.")
+                        Button(action: {
+                            BioAuth.tryBioAuth { (success) in
+                                if success {
+                                    self.authSuccess.toggle()
+                                } else {
+                                    self.showUnlock.toggle()
+                                }
+                            }
+                        }, label: {
+                            Text("Unlock")
+                        }).sheet(isPresented: $showUnlock, content: {
+                            EnterPasswordScreen(showUnlockScreen: self.$showUnlock, authSuccess: self.$authSuccess)
+                        })
+                        
+                        Spacer()
+                    }
+                } else {
+                    VStack {
+                        Text(note.note ?? "")
+                        Spacer()
+                    }
+                }
+            } else {
+                VStack {
+                    Text(note.note ?? "")
+                    Spacer()
+                }
+            }
+        }.padding()
     }
 }

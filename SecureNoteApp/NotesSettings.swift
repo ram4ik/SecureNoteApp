8149960f560f0c3a3e7a2d57e0e82e6edbd437ab
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-09-27 20:47:37 +0300

    NoteSettings

--- SecureNoteApp/NotesSettings.swift
+++ SecureNoteApp/NotesSettings.swift
@@ -6,10 +6,62 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct NotesSettings: View {
+    
+    @Environment(\.managedObjectContext) var moc
+    
+    @State private var showEnterPassword = false
+    @State private var passwordSuccess = false
+    @State private var showCreatePassword = false
+    
     var body: some View {
-        Text("Notes Settings")
+        VStack {
+            Color.clear.sheet(isPresented: $showCreatePassword, content: {
+                CreateNewPassword(createPassword: self.$showCreatePassword)
+            }).frame(width: 100, height: 20)
+            
+            Button("Update password") {
+                if PasswordHelper.isPasswordBlank {
+                    self.showCreatePassword.toggle()
+                } else {
+                    self.showEnterPassword.toggle()
+                }
+            }.sheet(isPresented: $showEnterPassword, content: {
+                EnterPasswordScreen(showUnlockScreen: self.$showEnterPassword, authSuccess: self.$passwordSuccess)
+                    .onDisappear() {
+                        if self.passwordSuccess {
+                            self.showCreatePassword.toggle()
+                        }
+                    }
+            })
+            
+            Divider()
+            
+            Button(action: {
+                self.deleteSecureRecords()
+                PasswordHelper.deletePassword()
+            }, label: {
+                Text("Reset Password")
+            })
+        }
+    }
+    
+    private func deleteSecureRecords() {
+        let request: NSFetchRequest<SecureNote> = SecureNote.fetchRequest()
+        request.predicate = NSPredicate.init(format: "isSecured==1")
+        
+        do {
+            let objects = try moc.fetch(request)
+            for item in objects {
+                moc.delete(item)
+            }
+            
+            try moc.save()
+        } catch {
+            print("\(error)")
+        }
     }
 }
 
